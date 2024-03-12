8f3585a60a4a830395b4a16360938f11102c4a5a
ichrhma <163158185+ichrhma@users.noreply.github.com>
2024-03-12 22:28:28 +0700

    Add files via upload

---
+++ task.js
@@ -0,0 +1,148 @@
+/**
+ * TODO 1.
+ * Buat array of object users (5 users).
+ * Object memiliki property: name, age, major.
+ * Note: Ubah var menggunakan JavaScript Modern.
+ */
+
+const users = [
+{
+  name: "Ichda",
+  age: "22",
+  major: "informatics",
+},
+
+{
+  name: "Jeno",
+  age: "24",
+  major: "informatics",
+},
+
+{
+  name: "Mingyu",
+  age: "27",
+  major: "informatics",
+},
+
+{
+  name: "Jaehyun",
+  age: "27",
+  major: "informatics",
+},
+
+{
+  name: "Isa",
+  age: "22",
+  major: "informatics",
+}
+];
+
+/**
+ * TODO 2
+ * Buat function all: Menampilkan semua data user.
+ * Hint: Gunakan for/for-of.
+ * Note: Ubah function menggunakan arrow function.
+ */
+function all() {
+  for (const user of users) {
+      console.log(`Users: ${user.name}, Age: ${user.age}, Major: ${user.major}`);
+  }}
+
+/**
+ * TODO 3
+ * Buat function store: Menambahkan user baru.
+ * Hint: Gunakan method push.
+ * Note: Ubah function menggunakan arrow function.
+ */
+function store(newUser) {
+  users.push(newUser);
+}
+const newUsers = {
+  name: 'Hoshi',
+  age: 29,
+  major: 'English'
+};
+
+store(newUsers);
+
+/**
+ * TODO 4.
+ * Buat function update: Mengedit data user.
+ * Hint: re-assign array.
+ * Note: Ubah function menggunakan arrow function.
+ */
+function update(index, user) {
+  if (index >= 0 && index < users.length) {
+    users[index] = { ...users[index], ...newUser }; 
+    console.log(`User  ${index} has been updated.`);
+} else {
+    console.log(`User ${index} is out of bounds.`);
+}
+}
+
+/**
+ * TODO 5.
+ * Buat function destroy: Menghapus data user.
+ * Hint: Gunakan method splice.
+ * Note: Ubah function menggunakan arrow function.
+ */
+function destroy(index) {
+  if (index >= 0 && index < users.length) {
+    users.splice(index, 1); 
+    console.log(`User at index ${index} has been destroyed.`);
+} else {
+    console.log(`Index ${index} is out of bounds.`);
+}
+}
+
+/**
+ * Function main.
+ * Jangan edit atau hapus function main.
+ * Function ini untuk testing task.
+ */
+
+const main = () => {
+  /**
+   * Test function index
+   */
+  console.log("# Get All Users");
+  all();
+
+  /**
+   * Test function store
+   */
+  console.log("# Add New User: Sabiq");
+  const newUser = {
+    name: "Sabiq",
+    age: 20,
+    major: "Informatics",
+  };
+  store(newUser);
+
+  /**
+   * Test function update
+   */
+  console.log("# Edit User: Isfa");
+  const editedIndex = 1;
+  const editedUser = {
+    name: "Isfhani Ghiyath",
+    age: 23,
+    major: "English",
+  };
+  update(editedIndex, editedUser);
+
+  /**
+   * Test function destroy
+   */
+  console.log("# Delete User: Nurul");
+  const deletedIndex = 2;
+  destroy(deletedIndex);
+};
+
+main();
+
+/**
+ * Jangan hapus exports.
+ * Exports ini untuk tujuan testing.
+ */
+module.exports = { users, all, store, update, destroy };
